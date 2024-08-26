ef7ae655b6cb9db6d85ba99693bb0b6104b86019
Le Anh Duc <ducvui2003@gmail.com>
2024-08-26 21:00:10 +0700

    spotless

--- identity-service/src/main/java/com/spring/delivery/config/properties/SecurityProperties.java
+++ identity-service/src/main/java/com/spring/delivery/config/properties/SecurityProperties.java
@@ -4,10 +4,11 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Component;
-import org.springframework.util.ResourceUtils;
 
 import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
 import com.google.api.client.json.gson.GsonFactory;
@@ -21,11 +22,13 @@ public class SecurityProperties {
 	private List<String> origins;
 	private List<String> methods;
 	private List<String> headers;
+	@Value("classpath:client_secret.json")
+	private Resource clientSecretsResource;
 
 	@Bean
 	public GoogleClientSecrets googleClientSecrets() throws IOException {
 		com.google.api.client.json.JsonFactory jsonFactory = new GsonFactory();
 		return GoogleClientSecrets.load(
-				jsonFactory, new FileReader(ResourceUtils.getFile("classpath:client_secret.json")));
+				jsonFactory, new FileReader(clientSecretsResource.getFile()));
 	}
 }
