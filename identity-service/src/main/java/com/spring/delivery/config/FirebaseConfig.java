adcb59a261f456d928e1194e386abcda6647a579
Le Anh Duc <ducvui2003@gmail.com>
2024-08-26 20:53:41 +0700

    spotless

--- identity-service/src/main/java/com/spring/delivery/config/FirebaseConfig.java
+++ identity-service/src/main/java/com/spring/delivery/config/FirebaseConfig.java
@@ -3,9 +3,6 @@ package com.spring.delivery.config;
 import java.io.IOException;
 import java.io.InputStream;
 
-
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -14,38 +11,40 @@ import org.springframework.core.io.Resource;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 @Configuration
 public class FirebaseConfig {
 
-    @Value("classpath:serviceAccountKey.json")
-    private Resource serviceAccountResource;
+	@Value("classpath:serviceAccountKey.json")
+	private Resource serviceAccountResource;
 
-    @Bean
-    public Gson gson() {
-        return new GsonBuilder()
-                .setLenient()
-                .create();
-    }
-    @Bean
-    public FirebaseApp initialize() {
-        try {
-            InputStream serviceAccount = serviceAccountResource.getInputStream();
+	@Bean
+	public Gson gson() {
+		return new GsonBuilder()
+				.setLenient()
+				.create();
+	}
+	@Bean
+	public FirebaseApp initialize() {
+		try {
+			InputStream serviceAccount = serviceAccountResource.getInputStream();
 
-            System.out.println(serviceAccount.available());
-            FirebaseOptions options = new FirebaseOptions.Builder()
-                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
-                    .setDatabaseUrl("https://delivery-react-native-ap-fa468-default-rtdb.firebaseio.com/")
-                    .build();
+			System.out.println(serviceAccount.available());
+			FirebaseOptions options = new FirebaseOptions.Builder()
+					.setCredentials(GoogleCredentials.fromStream(serviceAccount))
+					.setDatabaseUrl("https://delivery-react-native-ap-fa468-default-rtdb.firebaseio.com/")
+					.build();
 
-            if (FirebaseApp.getApps().isEmpty()) {
-                FirebaseApp.initializeApp(options);
-            }
+			if (FirebaseApp.getApps().isEmpty()) {
+				FirebaseApp.initializeApp(options);
+			}
 
-            return FirebaseApp.getInstance();
-        } catch (IOException e) {
-            e.printStackTrace();
-            return null;
-        }
-    }
+			return FirebaseApp.getInstance();
+		} catch (IOException e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
 }
