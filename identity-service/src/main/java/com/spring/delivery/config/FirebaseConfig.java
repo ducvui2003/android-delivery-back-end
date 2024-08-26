823c2308ea80c259f1fda2fad431b9b23d27ce0e
Le Anh Duc <ducvui2003@gmail.com>
2024-08-26 21:17:44 +0700

    use @Value to read file

--- identity-service/src/main/java/com/spring/delivery/config/FirebaseConfig.java
+++ identity-service/src/main/java/com/spring/delivery/config/FirebaseConfig.java
@@ -3,6 +3,7 @@ package com.spring.delivery.config;
 import java.io.IOException;
 import java.io.InputStream;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -14,37 +15,40 @@ import com.google.firebase.FirebaseOptions;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
+@Slf4j
 @Configuration
 public class FirebaseConfig {
 
-	@Value("classpath:serviceAccountKey.json")
-	private Resource serviceAccountResource;
+    @Value("classpath:serviceAccountKey.json")
+    private Resource serviceAccountResource;
 
-	@Bean
-	public Gson gson() {
-		return new GsonBuilder()
-				.setLenient()
-				.create();
-	}
-	@Bean
-	public FirebaseApp initialize() {
-		try {
-			InputStream serviceAccount = serviceAccountResource.getInputStream();
+    @Bean
+    public Gson gson() {
+        return new GsonBuilder()
+                .setLenient()
+                .create();
+    }
 
-			System.out.println(serviceAccount.available());
-			FirebaseOptions options = new FirebaseOptions.Builder()
-					.setCredentials(GoogleCredentials.fromStream(serviceAccount))
-					.setDatabaseUrl("https://delivery-react-native-ap-fa468-default-rtdb.firebaseio.com/")
-					.build();
+    @Bean
+    public FirebaseApp initialize() {
+        try {
+            InputStream serviceAccount = serviceAccountResource.getInputStream();
 
-			if (FirebaseApp.getApps().isEmpty()) {
-				FirebaseApp.initializeApp(options);
-			}
+            log.info("serviceAccount: {}", serviceAccount.available());
+            FirebaseOptions options = new FirebaseOptions.Builder()
+                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                    .setDatabaseUrl("https://delivery-react-native-ap-fa468-default-rtdb.firebaseio.com/")
+                    .build();
 
-			return FirebaseApp.getInstance();
-		} catch (IOException e) {
-			e.printStackTrace();
-			return null;
-		}
-	}
+            if (FirebaseApp.getApps().isEmpty()) {
+                FirebaseApp.initializeApp(options);
+                log.info("FirebaseApp initialized successfully");
+            }
+
+            return FirebaseApp.getInstance();
+        } catch (IOException e) {
+            log.error("Failed to initialize FirebaseApp", e);
+            return null;
+        }
+    }
 }
--- identity-service/src/main/java/com/spring/delivery/config/properties/SecurityProperties.java
+++ identity-service/src/main/java/com/spring/delivery/config/properties/SecurityProperties.java
@@ -1,19 +1,10 @@
 package com.spring.delivery.config.properties;
 
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Value;
+import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.Bean;
-import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Component;
 
-import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
-import com.google.api.client.json.gson.GsonFactory;
-
-import lombok.Data;
+import java.util.List;
 
 @Data
 @Component("securityProperties")
@@ -22,13 +13,5 @@ public class SecurityProperties {
 	private List<String> origins;
 	private List<String> methods;
 	private List<String> headers;
-	@Value("classpath:client_secret.json")
-	private Resource clientSecretsResource;
 
-	@Bean
-	public GoogleClientSecrets googleClientSecrets() throws IOException {
-		com.google.api.client.json.JsonFactory jsonFactory = new GsonFactory();
-		return GoogleClientSecrets.load(
-				jsonFactory, new FileReader(clientSecretsResource.getFile()));
-	}
 }
