315639ef1bdeb47a67def0540cfc3dc7359ee30e
Le Anh Duc <ducvui2003@gmail.com>
2024-08-26 20:48:39 +0700

    setLenient in gson, modifier in firebase config

--- identity-service/src/main/java/com/spring/delivery/config/FirebaseConfig.java
+++ identity-service/src/main/java/com/spring/delivery/config/FirebaseConfig.java
@@ -3,9 +3,11 @@ package com.spring.delivery.config;
 import java.io.IOException;
 import java.io.InputStream;
 
-import javax.annotation.PostConstruct;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.Resource;
 
@@ -16,25 +18,34 @@ import com.google.firebase.FirebaseOptions;
 @Configuration
 public class FirebaseConfig {
 
-	@Value("classpath:serviceAccountKey.json")
-	private Resource serviceAccountResource;
+    @Value("classpath:serviceAccountKey.json")
+    private Resource serviceAccountResource;
 
-	@PostConstruct
-	public void initialize() {
-		try {
-			InputStream serviceAccount = serviceAccountResource.getInputStream();
+    @Bean
+    public Gson gson() {
+        return new GsonBuilder()
+                .setLenient()
+                .create();
+    }
+    @Bean
+    public FirebaseApp initialize() {
+        try {
+            InputStream serviceAccount = serviceAccountResource.getInputStream();
 
-			System.out.println(serviceAccount.available());
-			FirebaseOptions options = new FirebaseOptions.Builder()
-				.setCredentials(GoogleCredentials.fromStream(serviceAccount))
-				.setDatabaseUrl("https://delivery-react-native-ap-fa468-default-rtdb.firebaseio.com/")
-				.build();
+            System.out.println(serviceAccount.available());
+            FirebaseOptions options = new FirebaseOptions.Builder()
+                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                    .setDatabaseUrl("https://delivery-react-native-ap-fa468-default-rtdb.firebaseio.com/")
+                    .build();
 
-			if (FirebaseApp.getApps().isEmpty()) {
-				FirebaseApp.initializeApp(options);
-			}
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
+            if (FirebaseApp.getApps().isEmpty()) {
+                FirebaseApp.initializeApp(options);
+            }
+
+            return FirebaseApp.getInstance();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
 }
