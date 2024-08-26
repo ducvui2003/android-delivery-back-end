512e673c3da7c1775a862903b327feb35f52c387
Le Anh Duc <ducvui2003@gmail.com>
2024-08-26 22:18:54 +0700

    use @Value to read file

--- identity-service/src/main/java/com/spring/delivery/config/GoogleConfig.java
+++ identity-service/src/main/java/com/spring/delivery/config/GoogleConfig.java
@@ -1,25 +1,25 @@
 package com.spring.delivery.config;
 
-import java.io.IOException;
-import java.io.InputStreamReader;
-
+import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.gson.GsonFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.Resource;
 
-import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
-import com.google.api.client.json.gson.GsonFactory;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 @Configuration
 public class GoogleConfig {
-	@Value("classpath:client_secret.json")
-	private Resource clientSecretsResource;
+    @Value("classpath:client_secret.json")
+    private Resource clientSecretsResource;
 
-	@Bean
-	public GoogleClientSecrets googleClientSecrets() throws IOException {
-		com.google.api.client.json.JsonFactory jsonFactory = new GsonFactory();
-		return GoogleClientSecrets.load(
-				jsonFactory, new InputStreamReader(clientSecretsResource.getInputStream()));
-	}
+    @Bean
+    public GoogleClientSecrets googleClientSecrets() throws IOException {
+        JsonFactory jsonFactory = GsonFactory.builder().setReadLeniency(true).build();
+        return GoogleClientSecrets.load(
+                jsonFactory, new InputStreamReader(clientSecretsResource.getInputStream()));
+    }
 }
