90a565bc1b6b6655dec8140d803c6b0354d133bd
Le Anh Duc <ducvui2003@gmail.com>
2024-08-26 20:11:27 +0700

    move ResourceUtil to get file config via @Value

--- identity-service/src/main/java/com/spring/delivery/config/FirebaseConfig.java
+++ identity-service/src/main/java/com/spring/delivery/config/FirebaseConfig.java
@@ -2,24 +2,29 @@ package com.spring.delivery.config;
 
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 
 import javax.annotation.PostConstruct;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.util.ResourceUtils;
 
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
+import org.springframework.core.io.Resource;
 
 @Configuration
 public class FirebaseConfig {
 
+	@Value("classpath:serviceAccountKey.json")
+	private Resource serviceAccountResource;
+
 	@PostConstruct
 	public void initialize() {
 		try {
-			FileInputStream serviceAccount =
-				new FileInputStream(ResourceUtils.getFile("classpath:serviceAccountKey.json"));
+			InputStream serviceAccount = serviceAccountResource.getInputStream();
 
 			System.out.println(serviceAccount.available());
 			FirebaseOptions options = new FirebaseOptions.Builder()
