1fd1796e17b008251fc69aac60e6db5b80b98f2a
Le Anh Duc <ducvui2003@gmail.com>
2024-08-31 16:38:48 +0700

    feat: xóa product service

--- product-service/src/main/java/com/spring/productservice/controller/CategoryController.java
+++
@@ -1,7 +0,0 @@
-package com.spring.productservice.controller;
-
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-public class CategoryController {
-}
--- product/src/config/axios.ts
+++
@@ -1,7 +0,0 @@
-/**
- * Author: Le Anh Duc
- * Email: ducvui2003@gmail.com
- * Phone number: +84 965809127
- * Created at: 29/8/24 - 2:57 pm
- * User: ducvui2003
- **/
