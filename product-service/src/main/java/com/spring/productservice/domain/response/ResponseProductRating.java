bf8ad0ad14a84d099db6b1bca2084440116b41c4
Nguyễn Đình Lam <21130416@st.hcmuaf.edu.vn>
2024-09-05 11:22:08 +0700

    fix: name project

--- product-service/src/main/java/com/spring/productservice/domain/response/ResponseProductRating.java
+++
@@ -1,9 +0,0 @@
-package com.spring.productservice.domain.response;
-
-public record ResponseProductRating(
-        Long productId,
-        Double totalRating,
-        Integer totalReview,
-        Double averageRating
-) {
-}
